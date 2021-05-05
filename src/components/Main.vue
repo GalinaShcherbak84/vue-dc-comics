9916f557d2dd73155a47eb545bda8fcb10c9966c
GalinaSherbak84 <galinasherbak@gmail.com>
2021-05-05 19:15:54 +0200

    main

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -28,6 +28,40 @@
     <div class="menu">
         <div class="container">
             <div class="left">
+                <div class="column">
+                    <h3>DC COMICS</h3>
+                    <p>Characters</p>
+                    <p>Comocs</p>
+                    <p>Movies</p>
+                    <p>TV</p>
+                    <p>Games</p>
+                    <p>Videos</p>
+                    <p>News</p>
+                    <h3>SHOP</h3>
+                    <p>Shop DC</p>
+                    <p>Shop DC Collectibles</p>
+                
+                </div>
+                <div class="column">
+                    <h3>DC COMICS</h3>
+                    <p>Characters</p>
+                    <p>Comocs</p>
+                    <p>Movies</p>
+                    <p>TV</p>
+                    <p>Games</p>
+                    <p>Videos</p>
+                    <p>News</p>
+                </div>
+                <div class="column">
+                    <h3>DC COMICS</h3>
+                    <p>Characters</p>
+                    <p>Comocs</p>
+                    <p>Movies</p>
+                    <p>TV</p>
+                    <p>Games</p>
+                    <p>Videos</p>
+                    <p>News</p>
+                </div>
 
             </div>
             <div class="right">
@@ -82,6 +116,24 @@ export default {
         .container{
             display: flex;
             justify-content: space-between;
+            .left{
+                display: flex;
+
+                .column{
+                    width:calc(100%/3);
+                    height: 3px;
+                    padding: 25px 10px;
+                    h3{
+                        color: white;
+                        margin: 10px 0;
+                    }
+                    p{
+                        color:gray;
+                        font-size: 14px;
+                        padding:3px 0;
+                    }
+                }
+            }
             .right{
                 img{
                     width: 500px;
